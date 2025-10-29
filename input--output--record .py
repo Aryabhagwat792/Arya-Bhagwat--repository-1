51fdcce0bc5e07025e8861bfcbc21f080b8127c4
Aryabhagwat792 <233760212+Aryabhagwat792@users.noreply.github.com>
2025-10-29 20:51:19 +0530

    Create input--output--record .py

---
+++ input--output--record .py
@@ -0,0 +1,17 @@
+Input
+Income: 5000
+Expense:2200
+Expense:500
+
+Output
+Total income: 5000
+Total expense: 2700
+Balance:2300
+
+
+
+Record
+Category        Amount       Description
+Income          5000         Salary
+Expense         2200         Groceries
+Expense         500          Electricity bill
